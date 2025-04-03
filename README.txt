ffc1e332e141cb2678a59c283c86962a58df604d
Samantha Misurda <smisurda@Jonathans-MBP.MG8702>
2025-04-03 16:42:21 -0400

    Added some minor styling and usability changes

--- README.txt
+++ README.txt
@@ -1,4 +1,12 @@
-/**
-	@author Samantha L. Misurda
-	README.txt
-*/
+## Samantha L. Misurda
+### Running the Application
+To launch the Streamlit application locally, execute `streamlit run linear-eq-practice.py`
+
+When the page loads, you will be prompted with an equation to solve. Add your response into the text box, and press enter. Continue this process until you have a correct answer. 
+
+If you wish to solve a new equation, either click the *New Equation* button, or refresh the page.
+
+------------
+
+
+The app is also accessible on Streamlit Community Cloud [here](https://linear-eq-practicepy-6svwzdmc8sdqtk2vqfg2qf.streamlit.app/ "here")
--- linear-eq-practice.py
+++ linear-eq-practice.py
@@ -22,41 +22,57 @@ def generate_equation():
 	    while (st.session_state.c-st.session_state.b) % st.session_state.a != 0:
 	    	st.session_state.b = np.random.randint(low=-10, high=10);
 	    	st.session_state.c = np.random.randint(low=-10, high=10);
-    
 
-# Setup the equation
-generate_equation()
+def clear_text():
+    st.session_state.number = st.session_state.widget
+    st.session_state.widget = ""
 
-# Determine how to format the equation in the form Ax +B = C 
-# When B is < 0, we want to print Ax - B = C, not Ax + - B = C
-if st.session_state.b < 0:
-	sign = '-'
-else:
-	sign = '+'
-b = abs(st.session_state.b)
-equation_string = f"{st.session_state.a} x {sign} {b} = {st.session_state.c}"
-st.title("Solve this linear equation:")
-st.latex(equation_string)
-number = st.text_input("X = ")
-
-# Check if the user has input a response, and that the response is a valid 
-# character (number), and if the response is correct
-if len(number) > 0:
-	# To determine how to print the help message based on operation
-	operation = "add" if sign == '-' else "subtract"
-	try:
-		if (int(number) * st.session_state.a + st.session_state.b) == st.session_state.c:
-			st.write(f"That's correct, great job! \n {operation.capitalize()}ing {st.session_state.b} {'to' if operation == 'add' else 'from'} each side and dividing both sides by {st.session_state.a} gives x = {number}")
-		else:
-			st.write(f"Sorry, that's not quite right. \n You should {operation} {st.session_state.b} {'to' if operation == 'add' else 'from'} both sides, and then divide {st.session_state.a} from both sides.")
-	except ValueError:
-		st.write("Invalid number. Please enter a positive or negative whole number.")
-
-
-if st.button("New equation"):
+def new_equation():
+	st.session_state.number = ""
+	number = ""
 	del st.session_state.a 
+	del st.session_state.b
+	del st.session_state.c
+	
 	generate_equation()
 
+def driver():
+
+	# Setup the equation
+	generate_equation()
+
+	# Determine how to format the equation in the form Ax +B = C 
+	# When B is < 0, we want to print Ax - B = C, not Ax + - B = C
+	if st.session_state.b < 0:
+		sign = '-'
+	else:
+		sign = '+'
+	b = abs(st.session_state.b)
+	equation_string = f"{st.session_state.a} x {sign} {b} = {st.session_state.c}"
+	st.title("Solve this linear equation:")
+	st.latex(equation_string)
+	left, right = st.columns([.04,.96],vertical_alignment="bottom")
+	with left:
+		st.write("X = ")
+	with right:	
+		st.text_input("",key='widget', on_change=clear_text)
+		number = st.session_state.get('number', '')
+
+	# Check if the user has input a response, and that the response is a valid 
+	# character (number), and if the response is correct
+	with st.empty():
+		if len(number) > 0:
+			# To determine how to print the help message based on operation
+			operation = "add" if sign == '-' else "subtract"
+			try: 
+				if (int(number) * st.session_state.a + st.session_state.b) == st.session_state.c:
+					st.write(f"<span style=\"color:green\"> 🥳 That's correct, great job! 🎉 \n {operation.capitalize()}ing {st.session_state.b} {'to' if operation == 'add' else 'from'} each side and dividing both sides by {st.session_state.a} gives x = {number}</span>",unsafe_allow_html=True)
+				else:
+					st.write(f"<span style=\"color:red\">⚠️ Sorry, {number} is not quite right. \n You should {operation} {st.session_state.b} {'to' if operation == 'add' else 'from'} both sides, and then divide {st.session_state.a} from both sides.<span>",unsafe_allow_html=True)
+			except ValueError:
+				st.write("Invalid number. Please enter a positive or negative whole number.")
 
 
-# Correct. Adding B to each side and dividing both sides by A gives x = C
+	st.button("Try another!", on_click=new_equation)
+
+driver();
