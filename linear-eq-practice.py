7d4b3f42f6383ad4ccdfb7fc9a39571f19e80da9
Samantha Misurda <smisurda@Jonathans-MBP.MG8702>
2025-04-08 16:13:00 -0400

    Added a help button. WOrking on animation now

--- linear-eq-practice.py
+++ linear-eq-practice.py
@@ -37,9 +37,12 @@ def new_equation():
 	generate_equation()
 
 def driver():
-
 	# Setup the equation
 	generate_equation()
+
+	# Initialize the counter variable in session state if it doesn't exist
+	if 'incorrect_count' not in st.session_state:
+	    st.session_state.incorrect_count = 0
 
 	# Determine how to format the equation in the form Ax +B = C 
 	# When B is < 0, we want to print Ax - B = C, not Ax + - B = C
@@ -68,7 +71,16 @@ def driver():
 				if (int(number) * st.session_state.a + st.session_state.b) == st.session_state.c:
 					st.write(f"<span style=\"color:green\"> 🥳 That's correct, great job! 🎉 \n {operation.capitalize()}ing {st.session_state.b} {'to' if operation == 'add' else 'from'} each side and dividing both sides by {st.session_state.a} gives x = {number}</span>",unsafe_allow_html=True)
 				else:
-					st.write(f"<span style=\"color:red\">⚠️ Sorry, {number} is not quite right. \n You should {operation} {st.session_state.b} {'to' if operation == 'add' else 'from'} both sides, and then divide {st.session_state.a} from both sides.<span>",unsafe_allow_html=True)
+					st.write(f"<span style=\"color:red\">⚠️ Sorry, {number} is not quite right. \n ",unsafe_allow_html=True)
+					
+					# Increment incorrect answer count
+					st.session_state.incorrect_count += 1
+
+					# Display a hint button if the user is continuing to struggle
+					if(st.session_state.incorrect_count >= 2):
+						st.write("Wrong :",st.session_state.incorrect_count)
+						if st.button('🛟 I need some help!'):
+							st.write(f"You should {operation} {st.session_state.b} {'to' if operation == 'add' else 'from'} both sides, and then divide {st.session_state.a} from both sides.")
 			except ValueError:
 				st.write("Invalid number. Please enter a positive or negative whole number.")
 
